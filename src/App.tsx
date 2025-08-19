91893eb8147157c8c9d7467409cf376478492530
Divya <divyadarsheel.dds@gmail.com>
2025-08-19 23:37:46 +0545

    fix: pages routing issue

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
       <BrowserRouter>
         <ThemeProvider>
           <Routes>
-            <Route path="/" element={<Index />} />
+            <Route path="/portfolio" element={<Index />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
